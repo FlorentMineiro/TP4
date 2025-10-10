4f7a0bcad97afbd7510edf674e29f06b630357b9
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-10-10 09:56:51 +0200

    Chargement de la base dans la tableview les secteurs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,12 @@
-module sio.tp4 {
+open module sio.tp4 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires jakarta.persistence;
-    requires org.hibernate.orm.core;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
     requires spring.context;
     requires spring.beans;
     requires spring.data.jpa;
-    requires spring.boot.autoconfigure;
-    requires spring.boot;
-    opens sio.tp4 to javafx.fxml;
-    exports sio.tp4;
-    exports sio.tp4.controllers;
-    opens sio.tp4.controllers to javafx.fxml;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires spring.data.commons;
 }
